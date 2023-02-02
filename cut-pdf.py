6bee21877e0bcc629c155a46cd9d307486d8c236
EnigmaK9 <enigmak9@protonmail.com>
2023-02-02 01:10:16 -0600

    updated code, it works

--- cut-pdf.py
+++ cut-pdf.py
@@ -1,39 +1,21 @@
 import PyPDF2
+import os
 
-def split_pdf(file_path, page_limit=30):
-    # Open the PDF file
-    pdf_file = open(file_path, 'rb')
-    pdf_reader = PyPDF2.PdfFileReader(pdf_file)
+def select_file():
+    file_path = input("Enter the file path: ")
+    num_pages = int(input("Enter the number of pages per split file: "))
+    split_pdf(file_path, num_pages)
 
-    # Get the number of pages in the PDF file
-    num_pages = pdf_reader.numPages
+def split_pdf(file_path, num_pages):
+    with open(file_path, 'rb') as file:
+        pdf_reader = PyPDF2.PdfReader(file)
+        for i in range(0, len(pdf_reader.pages), num_pages):
+            pdf_writer = PyPDF2.PdfWriter()
+            for page in pdf_reader.pages[i:i + num_pages]:
+                pdf_writer.add_page(page)
+            output_file = f"split_{i//num_pages}.pdf"
+            with open(output_file, 'wb') as output:
+                pdf_writer.write(output)
 
-    # Calculate the number of stacks
-    num_stacks = num_pages // page_limit
-
-    # Loop through each stack
-    for i in range(num_stacks):
-        # Create a PDF file for the stack
-        stack_file = open(f"stack_{i}.pdf", 'wb')
-        pdf_writer = PyPDF2.PdfFileWriter()
-
-        # Add pages to the stack
-        for j in range(page_limit):
-            page_num = i * page_limit + j
-            if page_num >= num_pages:
-                break
-            page = pdf_reader.getPage(page_num)
-            pdf_writer.addPage(page)
-
-        # Write the stack to the file
-        pdf_writer.write(stack_file)
-
-        # Close the stack file
-        stack_file.close()
-
-    # Close the PDF file
-    pdf_file.close()
-
-# Example usage
-split_pdf('sample.pdf')
+select_file()
 
